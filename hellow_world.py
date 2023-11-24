c355302d63bd9e2127217ee2f492f425dabcbe7e
Charlotte Salcedo <salcedocharlotte@gmail.com>
2023-11-24 08:40:20 -0800

    hello world
    week 1 hello world

---
+++ hellow_world.py
@@ -0,0 +1,10 @@
+# Charlotte Salcedo Python Class CIS261
+# Tutorial: How to Add a GitHub Post in Visual Studio Code
+
+# This is a basic Python script that prints "Hello World" to the console.
+# It's often used as a simple demonstration of a programming language's syntax.
+
+# The print function outputs text to the console.
+print("Hello World")
+
+# End of the script
